232ebf16dc837eb6704b82ae477e31900e8808a7
Benjamin M. <reallybaddeveloper@gmail.com>
2024-11-21 19:38:20 -0500

    Add files via upload

---
+++ InfiniteRounds/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace InfiniteRounds
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony harmony = new(MyPluginInfo.PLUGIN_GUID);
+            harmony.PatchAll(typeof(Patches));
+        }
+    }
+    public class Patches
+    {
+        [HarmonyPatch(typeof(GameManager), nameof(GameManager.Update))]
+        [HarmonyPostfix]
+        public static void Update(GameManager __instance)
+        {
+            // Plugin.Logger.LogInfo(__instance.timer.ToString());
+            __instance.timer = 150f;
+            __instance.timerText.text = "∞";
+        }
+    }
+}
